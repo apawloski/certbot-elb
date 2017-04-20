e6edbdf238fdb443d1d09aeeee65e017f6c36c02
Andrew Pawloski <apawloski@gmail.com>
2017-04-20 17:09:58 -0400

    Separated cert upload from alb update

--- cert.sh
+++ cert.sh
@@ -3,25 +3,29 @@
 # There are Let's Encrypt API limits!
 
 DOMAIN="$1"
-ALB_NAME="$2"
-HTTPS_LISTENER_ARN="$3"
+EMAIL="$2"
 
-EMAIL="admin@example.com"
+# Not sure what to do with these yet
+ALB_NAME="$3"
+HTTPS_LISTENER_ARN="$4"
 
 # Check inputs. Or something.
 
 # Expects to find the PKI components at /etc/letsencrypt/live/$DOMAIN/
-function update_alb {
+function upload_cert_to_aws {
   # Ugly way to get AWS-name-friendly string of expiration date
   exp_date=$(openssl x509 -enddate -noout -in /etc/letsencrypt/live/$DOMAIN/cert.pem | cut -f2 -d= | awk -v OFS='-' '$1=$1' | cut -f1,2,4 -d-)
   cert_aws_name="${DOMAIN}-until-${exp_date}"
 
   # Upload to cert to AWS
-  cert_arn=$(aws iam upload-server-certificate  --server-certificate-name "$cert_aws_name" \
+  export cert_arn=$(aws iam upload-server-certificate  --server-certificate-name "$cert_aws_name" \
                                      --certificate-body file:///etc/letsencrypt/live/$DOMAIN/cert.pem  \
                                      --certificate-chain file:///etc/letsencrypt/live/$DOMAIN/chain.pem \
                                      --private-key file:///etc/letsencrypt/live/$DOMAIN/privkey.pem \
                                      | jq '.ServerCertificateMetadata.Arn')
+}
+
+function update_alb {
 
   aws elbv2 modify-listener --listener-arn "$HTTPS_LISTENER_ARN" \
                             --certificates "$cert_arn"
@@ -57,7 +61,8 @@ function main {
 
   # Certbot ran succesfully. Was anything created?
   if [ -e /etc/letsencrypt/live/$DOMAIN/cert.pem ]; then
-    echo "New certificate created for $DOMAIN. This is where we might update a load balancer."
+      echo "New certificate created for $DOMAIN. This is where we might update a load balancer."
+      upload_cert_to_aws
 #    update_alb
   else
     echo "Certbot ran succesfully, but no certificate generated for $DOMAIN. Probably because the cert was not yet ready for renewal."
